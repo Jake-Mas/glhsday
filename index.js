1ac1bee01d74a07b38ad15b62e496a098cdc1de5
Jake <jakehollmann@icloud.com>
2024-01-24 20:08:56 -0500

    Grammar + added offset

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 function getDay(){
+    let offset = 0
     let day = new Date();
     let currentDate = day.getDate()
     const compareDate = new Date("January 8, 2024 00:00:00").getDate();
     let weekday;
 
-    if (day.getDay() == 6) { // saturday
+    if (day.getDay() == 6) {
         currentDate = currentDate + 2
-    } else if (day.getDay() == 0) { // sunday
+    } else if (day.getDay() == 0) { 
         currentDate = currentDate + 1
     } else {
         weekday = true
@@ -18,9 +19,10 @@ function getDay(){
     daydiff = daydiff - weekends;
 
     let schoolday = (daydiff % 4) + 1;
+    schoolday = schoolday + offset
     if (weekday == true){
-        document.getElementById("day").innerHTML = "On est jour " + schoolday;
+        document.getElementById("day").innerHTML = "Il est jour " + schoolday;
     } else{
-        document.getElementById("day").innerHTML = "On va être jour " + schoolday + " lundi.";
+        document.getElementById("day").innerHTML = "Il sera jour " + schoolday + " lundi.";
     }
 }
