e245f80e8b90d3d74f4ce6bae2c2379ade8866c0
Jake <jakehollmann@icloud.com>
2024-03-09 09:09:13 -0500

    change for march break

--- index.js
+++ index.js
@@ -24,8 +24,9 @@ function getDay(){
         schoolday += 4
     }
     if (weekday == true) {
-        document.getElementById("day").innerHTML = "Nous sommes jour " + schoolday;
+        // document.getElementById("day").innerHTML = "Nous sommes jour " + schoolday;
     } else {
-        document.getElementById("day").innerHTML = "Nous serons jour " + schoolday + " lundi.";
+        // document.getElementById("day").innerHTML = "Nous serons jour " + schoolday + " lundi.";
     }
+    document.getElementById("day").innerHTML = "Joyeuses vacances! Jour 2 au retour.";
 }
